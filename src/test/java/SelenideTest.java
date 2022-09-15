05b4a6be67a44cd834a4742206eed6ced05686f8
Anna Yurasova <anna.yurasova@ssi-ru.local>
2022-09-15 21:29:35 +0400

    add test

---
+++ src/test/java/SelenideTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SelenideTest {
+
+    @Test
+    void test() {
+        open("http://localhost:9999/");
+        $x("//input[@placeholder='Город']").setValue("Самара");
+        $("span[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A");
+        $("span[data-test-id='date'] input").sendKeys(Keys.DELETE);
+
+        LocalDate date = LocalDate.now().plusDays(4);
+        String localDate = date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("span[data-test-id='date'] input").setValue(localDate);
+
+
+        $x("//input[@name='name']").setValue("Петров Александр");
+        $x("//input[@name='phone']").setValue("+79278764312");
+        $(By.className("checkbox__box")).click();
+        $x("//*[text()='Забронировать']").click();
+
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(20));
+        $(byText("Встреча успешно забронирована на " + localDate));
+    }
+}
